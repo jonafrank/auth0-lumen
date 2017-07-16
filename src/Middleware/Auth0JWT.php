dfc0111df9e99627a4c18f77fe5e754d30c34b64
Jonatan Frank <jonatan@stensul.com>
2017-07-16 12:22:40 -0300

    add JWT middleware

---
+++ src/Middleware/Auth0JWT.php
@@ -0,0 +1,90 @@
+<?php
+namespace Auth0\Lumen\Middleware;
+
+use Closure;
+use Auth0\Lumen\Contract\Auth0UserRepository;
+use Auth0\SDK\Exception\CoreException;
+use Auth0\SDK\Exception\InvalidTokenException;
+use Illuminate\Contracts\Auth\Factory as Auth;
+
+class Auth0JWT
+{
+    protected $userRepository;
+
+    protected $auth;
+
+    /**
+     * Auth0JWTMiddleware constructor.
+     *
+     * @param Auth0UserRepository $userRepository
+     */
+    public function __construct(Auth0UserRepository $userRepository, Auth $auth)
+    {
+        $this->userRepository = $userRepository;
+    }
+
+    /**
+     * @param $request
+     *
+     * @return string
+     */
+    protected function getToken($request)
+    {
+        // Get the encrypted user JWT
+        $authorizationHeader = $request->header('Authorization');
+
+        return trim(str_replace('Bearer ', '', $authorizationHeader));
+    }
+
+    /**
+     * @param $token
+     *
+     * @return bool
+     */
+    protected function validateToken($token)
+    {
+        return $token !== '';
+    }
+
+    /**
+     * @param $request
+     * @param Closure $next
+     *
+     * @return mixed
+     */
+     public function handle($request, Closure $next)
+     {
+         $auth0 = app()->make('auth0');
+
+         $token = $this->getToken($request);
+
+         if (!$this->validateToken($token)) {
+             return respnse('Unauthorized user', 401);
+         }
+
+         if ($token) {
+             try {
+                 $jwtUser = $auth0->decodeJWT($token);
+             } catch (CoreException $e) {
+                 return response('Unauthorized user', 401);
+             } catch (InvalidTokenException $e) {
+                 return response('Unauthorized user', 401);
+             }
+
+             // if it does not represent a valid user, return a HTTP 401
+             $user = $this->userRepository->getUserByDecodedJWT($jwtUser);
+
+             if (!$user) {
+                 return response('Unauthorized user', 401);
+             }
+
+             // lets log the user in so it is accessible
+             $this->auth->viaRequest('api', function() use ($user) {
+                 return $user;
+             });
+         }
+
+         // continue the execution
+         return $next($request);
+     }
+}
